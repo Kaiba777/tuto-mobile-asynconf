0abf2ba2bff85a87c08e8fc6d481b1e46ebade27
Kaiba 777 <sawadogomartial222@gmail.com>
2024-05-13 16:42:05 +0000

    Page d'acceuil

---
+++ android/app/src/main/kotlin/fr/asynconf/asynconf/MainActivity.kt
@@ -0,0 +1,5 @@
+package fr.asynconf.asynconf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Asynconf 2022"),
+        ),
+        body:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                "asset/images/logo.svg",
+                color: Colors.deepOrange,
+              ),
+              const Text("Asynconf 2022",
+                style: TextStyle(
+                  fontSize: 42,
+                  fontFamily: 'Poppins'
+                ),
+              ),
+              const Text("Salon virtuel de l'informatique. Du 27 au 29 octobre 2022",
+              style: TextStyle(fontSize: 24),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
